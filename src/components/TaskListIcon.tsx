9238909da4821a482ce90f2defecac18f2757d9a
Christopher Connolly <425070+chaosloth@users.noreply.github.com>
2023-05-29 14:27:45 +1000

    Add ANZ logo

--- src/components/TaskListIcon.tsx
+++ src/components/TaskListIcon.tsx
@@ -22,35 +22,45 @@ function isCurrentTask(task: ITask | undefined): boolean {
 }
 
 const imgStyle = {
-  height: '100%'
-}
+  height: "100%",
+};
 
 const TaskListItemIcon = (props: TaskContextProps) => {
   console.log("ATTRIBUTES", props.task?.attributes);
 
   const task = props.task;
 
-  const backgroundColor = isCurrentTask(task) ? 'rgb(225, 227, 234)' : undefined
+  const backgroundColor = isCurrentTask(task)
+    ? "rgb(225, 227, 234)"
+    : undefined;
 
   const divStyle = {
-    width: '52px',
-    height: '52px',
-    padding: '6px',
-    borderColor: 'rgb(202, 205, 216)',
-    borderStyle: 'solid',
-    borderWidth: '0px 0px 1px',
-    backgroundColor
-  }
+    width: "52px",
+    height: "52px",
+    padding: "6px",
+    borderColor: "rgb(202, 205, 216)",
+    borderStyle: "solid",
+    borderWidth: "0px 0px 1px",
+    backgroundColor,
+  };
 
-  const cbaLogoUrl = "http://www.commbank.com.au/content/dam/commbank/commBank-logo.svg";
-  const bwLogoUrl = "http://www.bankwest.com.au/content/dam/bankwest/web-assets/images/global/logo/logo-bankwest-footer.png";
+  const anzLogoUrl = "https://companieslogo.com/img/orig/ANZ.AX-29f7eb8a.png";
+  const cbaLogoUrl =
+    "http://www.commbank.com.au/content/dam/commbank/commBank-logo.svg";
+  const bwLogoUrl =
+    "http://www.bankwest.com.au/content/dam/bankwest/web-assets/images/global/logo/logo-bankwest-footer.png";
 
-  return (<>
-    <div style={divStyle}>
-      <img src={props.task?.attributes.brand === "bw" ? bwLogoUrl : cbaLogoUrl} alt="Company logo" style={imgStyle} />
-    </div>
-  </>)
-
-}
+  return (
+    <>
+      <div style={divStyle}>
+        <img
+          src={props.task?.attributes.brand === "bw" ? bwLogoUrl : anzLogoUrl}
+          alt="Company logo"
+          style={imgStyle}
+        />
+      </div>
+    </>
+  );
+};
 
 export default withTaskContext(TaskListItemIcon);
